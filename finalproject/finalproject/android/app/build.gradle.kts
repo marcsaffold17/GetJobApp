cbc8f2d838d604262510270ddbf9ee6cbfb6b29e
Athish <amita004@d.umn.edu>
2025-04-29 21:02:30 -0500

    added firebase things to gradle files

--- finalproject/finalproject/android/app/build.gradle.kts
+++ finalproject/finalproject/android/app/build.gradle.kts
@@ -3,8 +3,11 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
-
+dependencies {
+    implementation("com.google.firebase:firebase-bom:33.8.0")
+}
 android {
     namespace = "com.example.finalproject"
     compileSdk = flutter.compileSdkVersion
