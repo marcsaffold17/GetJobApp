06808a5f4927bc1133b6ca45e20a99089241d13c
msaffold <saffo026@d.umn.edu>
2025-05-02 15:06:57 -0500

    Start of average salary screen

--- finalproject/finalproject/lib/view/compareSalariesCities_view.dart
+++ finalproject/finalproject/lib/view/compareSalariesCities_view.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import '../presenter/SWE_List_presenter.dart';
+import '../model/SWE_List_model.dart';
 
-class CompareCitiesScreen extends StatefulWidget {
-  @override
-  _CompareCitiesScreen createState() => _CompareCitiesScreen();
-}
+class CompareCitiesScreen extends StatelessWidget {
+  final List<JobEntry> jobs;
 
-class _CompareCitiesScreen extends State<CompareCitiesScreen> {
+  CompareCitiesScreen({required this.jobs});
+
+  Map<String, double> computeAverageSalaryByCity(List<JobEntry> jobs) {
+    final Map<String, List<double>> citySalaries = {};
+
+    for (var job in jobs) {
+      final salary = job.parsedSalary;
+      if (salary != null) {
+        citySalaries.putIfAbsent(job.location, () => []).add(salary);
+      }
+    }
+
+    return citySalaries.map((city, salaries) {
+      final average = salaries.reduce((a, b) => a + b) / salaries.length;
+      return MapEntry(city, average);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final citySalaryMap = computeAverageSalaryByCity(jobs);
+    final sortedCities = citySalaryMap.entries.toList()
+      ..sort((a, b) => b.value.compareTo(a.value));
+
     return Scaffold(
-      appBar: AppBar(title: Text('Compare Cities'),),
-      body: Center(
-        child: Text(
-          "Hello World!!"
-        ),
+      appBar: AppBar(title: Text('Average Salary By City')),
+      body: ListView.builder(
+        itemCount: sortedCities.length,
+        itemBuilder: (context, index) {
+          final entry = sortedCities[index];
+          return ListTile(
+            title: Text(entry.key),
+            trailing: Text('\$${entry.value.toStringAsFixed(1)}k'),
+          );
+        },
       ),
     );
   }
