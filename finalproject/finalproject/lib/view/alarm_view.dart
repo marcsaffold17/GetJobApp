bd818a46fa320acf765e50808663644bbfc9d586
msaffold <saffo026@d.umn.edu>
2025-05-04 12:02:01 -0500

    UI updates to alarm screen

--- finalproject/finalproject/lib/view/alarm_view.dart
+++ finalproject/finalproject/lib/view/alarm_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../presenter/alarm_presenter.dart';
 import '../model/alarm_model.dart';
 
@@ -36,6 +37,7 @@ class _AlarmScreenState extends State<AlarmScreen> implements AlarmView {
     final model = AlarmModel(
       id: DateTime.now().millisecondsSinceEpoch.toString(),
       dateTime: _selectedDateTime,
+      title: 'Alarm', // Change this to user inputted event name
     );
     _presenter.setAlarm(model);
   }
@@ -55,26 +57,111 @@ class _AlarmScreenState extends State<AlarmScreen> implements AlarmView {
 
   @override
   Widget build(BuildContext context) {
+
+    // Alarms are grouped by date
+    final alarmsByDate = <DateTime, List<AlarmModel>>{};
+    for (var alarm in _alarms) {
+      final date = DateTime(alarm.dateTime.year, alarm.dateTime.month, alarm.dateTime.day);
+      alarmsByDate.putIfAbsent(date, () => []).add(alarm);
+    }
+
+    // Dates sorted by most to least recent
+    final sortedDates = alarmsByDate.keys.toList()
+      ..sort((b, a) => b.compareTo(a));
+
     return Scaffold(
-      appBar: AppBar(title: Text('Set Alarm')),
+      backgroundColor: const Color.fromARGB(255, 244, 243, 240),
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 230, 230, 226),
+        elevation: 2,
+        centerTitle: true,
+        title: const Text(
+          'Alarms',
+          style: TextStyle(
+            fontFamily: 'inter',
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Color.fromARGB(255, 0, 43, 75),
+          ),
+        ),
+        iconTheme: const IconThemeData(color: Color.fromARGB(255, 0, 43, 75)),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            ElevatedButton(onPressed: () => _selectDateTime(context), child: Text('Pick Alarm Time')),
+
+            // Temporarily removing "pick alarm time" and "set alarm" buttons
+            /*
+            ElevatedButton(
+              onPressed: () => _selectDateTime(context),
+              child: Text('Pick Alarm Time'),
+            ),
             SizedBox(height: 16),
             ElevatedButton(onPressed: _setAlarm, child: Text('Set Alarm')),
             SizedBox(height: 16),
-            Expanded( // Important!
+            */
+
+            Expanded(
               child: _alarms.isEmpty
-                  ? Text("No alarms set.")
+                  ? Center(child: Text("No alarms set."))
                   : ListView.builder(
-                itemCount: _alarms.length,
+                itemCount: sortedDates.length,
+                padding: const EdgeInsets.all(12.0),
                 itemBuilder: (context, index) {
-                  final alarm = _alarms[index];
-                  return ListTile(
-                    title: Text('Alarm ID: ${alarm.id}'),
-                    subtitle: Text('Time: ${alarm.dateTime}'),
+                  final date = sortedDates[index];
+                  final dailyAlarms = alarmsByDate[date]!;
+
+                  return Card(
+                    color: const Color.fromARGB(255, 230, 230, 226),
+                    margin: const EdgeInsets.symmetric(vertical: 6),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    elevation: 3,
+                    child: ExpansionTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side: BorderSide.none,
+                      ),
+                      collapsedShape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        side: BorderSide.none,
+                      ),
+                      backgroundColor: const Color.fromARGB(255, 230, 230, 226),
+                      title: Text(
+                        DateFormat('EEEE, MMMM d, y').format(date),
+                        style: const TextStyle(
+                          fontFamily: 'inter',
+                          color: Color.fromARGB(255, 0, 43, 75),
+                        ),
+                      ),
+                      children: [
+                        const Divider(
+                          color: Color.fromARGB(255, 0, 43, 75),
+                          thickness: 1,
+                        ),
+                        ...dailyAlarms.map((alarm) => ListTile(
+                          dense: true,
+                          title: Text(
+                            alarm.title ?? 'Alarm',
+                            style: const TextStyle(
+                              fontFamily: 'JetB',
+                              fontSize: 12,
+                              color: Color.fromARGB(255, 17, 84, 116),
+                            ),
+                          ),
+                          trailing: Text(
+                            DateFormat('hh:mm a').format(alarm.dateTime),
+                            style: const TextStyle(
+                              fontFamily: 'JetB',
+                              fontSize: 12,
+                              color: Color.fromARGB(255, 34, 124, 157),
+                            ),
+                          ),
+                        )),
+                      ],
+                    ),
                   );
                 },
               ),
@@ -82,7 +169,6 @@ class _AlarmScreenState extends State<AlarmScreen> implements AlarmView {
           ],
         ),
       ),
-
     );
   }
 
